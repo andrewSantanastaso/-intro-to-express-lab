218e9f88e71971cc1bc6185c8d18a721d0294e09
andrewSantanastaso <andrewsantanastaso@gmail.com>
2024-06-28 10:30:50 -0400

    shoes route complete

--- server.js
+++ server.js
@@ -4,6 +4,16 @@ const collectibles = [
     { name: 'autographed picture of a dog', price: 10 },
     { name: 'vintage 1970s yogurt SOLD AS-IS', price: 0.99 }
 ];
+const shoes = [
+    { name: "Birkenstocks", price: 50, type: "sandal" },
+    { name: "Air Jordans", price: 500, type: "sneaker" },
+    { name: "Air Mahomeses", price: 501, type: "sneaker" },
+    { name: "Utility Boots", price: 20, type: "boot" },
+    { name: "Velcro Sandals", price: 15, type: "sandal" },
+    { name: "Jet Boots", price: 1000, type: "boot" },
+    { name: "Fifty-Inch Heels", price: 175, type: "heel" }
+];
+
 
 
 const app = express()
@@ -33,6 +43,34 @@ app.get('/collectibles/:idx', (req, res) => {
         res.send('This item is not yet in stock')
     }
 })
+
+app.get('/shoes', (req, res) => {
+    let minprice = req.query.minprice
+    let maxprice = req.query.maxprice
+    let type = req.query.type
+    let filteredOutput = shoes
+    if (minprice) {
+        filteredOutput = filteredOutput.filter((shoe) =>
+            shoe.price >= minprice
+        )
+
+    }
+    if (maxprice) {
+        filteredOutput = filteredOutput.filter((shoe) =>
+            shoe.price <= maxprice
+        )
+
+    }
+    if (type) {
+        filteredOutput = filteredOutput.filter((shoe) =>
+            shoe.type === type)
+    }
+    res.send(filteredOutput)
+}
+
+
+
+)
 
 app.listen(3000, () => {
     console.log('Listening on 3000')
